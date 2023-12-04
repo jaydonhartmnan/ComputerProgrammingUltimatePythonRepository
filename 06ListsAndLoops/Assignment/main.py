11a0864186a29a335d4d0ac4a0f82ebbdb4012b2
Jaydon hartman <27jaydonhartman@usd266.com>
2023-12-04 10:19:12 -0600

    incompleted

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -5,8 +5,8 @@ def count_failing_grades(grades):
           count= count + 1
     return count
     
-inputlist = [40, 80, 71, 26, 93]
-returnValue = count_failing_grades(inputlist)
+inputlist1 = [40, 80, 71, 26, 93]
+returnValue = count_failing_grades(inputlist1)
 print("There are this many failing grades",(returnValue))
 print("######################################")
 print("")
@@ -19,8 +19,8 @@ def count_act_score(list):
           validact= validact + 1
    return validact 
 
-inputlist = [-5, 24, 31, 56, 25]
-returnValue = count_act_score(inputlist)
+inputlist2 = [-5, 24, 31, 56, 25]
+returnValue = count_act_score(inputlist2)
 print("This many valid act scores=",(returnValue))
 print("######################################")
 print("")
@@ -31,8 +31,8 @@ def num_sum(list):
     total = total + number 
    return total
 
-inputlist = [-5, 24, 31, 56, 25]
-returnValue = num_sum(inputlist)
+inputlist3 = [-5, 24, 31, 56, 25]
+returnValue = num_sum(inputlist3)
 print("Number sum=",(returnValue))
 print("######################################")
 print("")
@@ -49,8 +49,8 @@ def avergage_act_score(list):
       mean= validact/count
       return mean
    
-inputlist = [-5, 24, 31, 56, 25]
-returnValue = avergage_act_score(inputlist)
+inputlist4 = [-5, 24, 31, 56, 25]
+returnValue = avergage_act_score(inputlist4)
 print("Avergage act score=",(returnValue))
 print("######################################")
 print("")
@@ -61,8 +61,8 @@ def all_true(list):
         return False
    return True 
    
-inputlist = ["True","False","True","True"]
-returnValue = all_true(inputlist)
+inputlist5 = ["True","False","True","True"]
+returnValue = all_true(inputlist5)
 print("True,False,True,True-All true=",(returnValue))
 print("######################################")
 print("")
@@ -72,8 +72,8 @@ def any_true(list):
       if item in ["True"]:
          return True
       
-inputlist = ["True","False","True","True"]
-returnValue = any_true(inputlist)
+inputlist6 = ["True","False","True","True"]
+returnValue = any_true(inputlist6)
 print("True,False,True,True-Any true=",(returnValue))
 print("######################################")
 print("")
@@ -91,9 +91,9 @@ def mostly_true(list):
       return True 
    else:
       return False
-
-inputlist = ["Falsee","False","False","True"]
-returnValue = mostly_true(inputlist)
+print("mostly tue demo")
+inputlist7 = ["Falsee","False","False","True"]
+returnValue = mostly_true(inputlist7)
 print("False,False,False,True-mostly true=",(returnValue))
 print("######################################")
 print("")
@@ -104,13 +104,57 @@ def has_vowel(input):
          return True
       else:
          return False
-inputlist = ["e"]
-inputlist1 = ["t"]
-
-returnValue = has_vowel(inputlist)
+inputlist8 = ["e"]
+inputlist9 = ["t"]
+print("has a vowel demo")
+returnValue = has_vowel(inputlist8)
 print("e=",(returnValue))
-returnValue1 = has_vowel(inputlist)
-print("a=",(returnValue1))
+returnValue1 = has_vowel(inputlist9)
+print("z=",(returnValue1))
 
 print("######################################")
 print("")
+
+def all_the_same(list):
+   first= list[0]
+   for num in list:
+      if first!= list[0]:
+         return False
+      else:
+         return True
+
+
+print("all the same demo")
+print("2,2,2",all_the_same([2,2,2]))
+print("1,2,3",all_the_same([1,2,3]))
+print("######################################")
+print("")
+
+def increasing(list):
+   for num in list:
+      if list[0]<list[1] and list[1]<list[2]:
+         return True
+      else:
+         return False
+print("increasing demo")
+print("2,2,2",increasing([2,2,2]))
+print("1,2,3",increasing([1,2,3]))
+print("######################################")
+print("")
+
+def incrimating(list):
+   for num in list:
+      if list[1]== (list[0]-1) or list[1]==(list[0]+1):
+         return True
+      else:
+         return False
+
+print("incimating demo")     
+print("2,2",incrimating([2,2]))
+print("1,2,",incrimating([1,2]))
+print("######################################")
+print("")
+
+      
+def adjacant(list):
+   if 
